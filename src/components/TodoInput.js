4d9290709cf58199f8153fbe4a15db521d008297
Miraculous-V <dusworld@gmail.com>
2020-04-04 01:13:34 +0100

    finished add todo item component

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,28 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
 	render() {
+		const { item, handleChange, handleSubmit, editItem } = this.props;
 		return (
-			<div>
-				<h1>Hello from todo input</h1>
+			<div className='card card-body my-3'>
+				<form onSubmit={handleSubmit}>
+					<div className='input-group'>
+						<div className='input-group-prepend'>
+							<div className='input-group-text bg-primary text-white'>
+								<i className='fas fa-book'></i>
+							</div>
+						</div>
+						<input
+							type='text'
+							className='form-control'
+							placeholder='Add to do Item'
+							value={item}
+							onChange={handleChange}
+						/>
+					</div>
+					<button className='btn btn-block btn-primary mt-3 text-uppercase'>
+						Add Todo
+					</button>
+				</form>
 			</div>
 		);
 	}
